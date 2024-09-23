17ba11c8742ac02b0158640899bfdd4abcdfccee
Aaronfel <feldmanaaron406@gmail.com>
2024-09-22 21:42:32 -0300

    fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -65,9 +65,9 @@ const Card = ({ title, src, description, techs, company, link }: CardProps) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
